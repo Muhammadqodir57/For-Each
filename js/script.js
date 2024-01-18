8a22cd3f35b7f833f405bcaa2a468198e8251998
Muhammadqodir_57 <mirzamatovanasiba@gmail.com>
2024-01-18 17:46:24 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let numbers = [
+//     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+// ]
+// numbers.forEach((item, index)=>{
+//     console.log(item,);
+// })
+arr = [235245,245345346, 21, 12442, 246457 ,234 ];
+
+arr.forEach(item => {
+    if(item < 97687585675756774) {
+        console.log(item);
+    }
+});
